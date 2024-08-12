ce2e9238ce6afc9ce77be90393bec30a167f6c25
Arash-Azarpoor <azarpoor81@gmail.com>
2024-08-12 18:33:38 +0330

    Add form validation
    Co-authored-by: Armin <arminmow@gmail.com>

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
-import {RouterModule} from "@angular/router";
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule, NgIf , NgClass],
+  providers: [FormBuilder, Validators],
   templateUrl: './sign-in.component.html',
-  styleUrl: './sign-in.component.scss'
+  styleUrl: './sign-in.component.scss',
 })
 export class SignInComponent {
-action = 'sing-up'
+  signInForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.signInForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.signInForm.valid) {
+      console.log(this.signInForm.value);
+    }
+  }
 }
