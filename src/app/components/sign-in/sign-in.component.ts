dcd7bf92d469779db3d3a7e2ff14ffa8d96bdb99
Arash-Azarpoor <azarpoor81@gmail.com>
2024-08-12 21:23:12 +0330

    Add inputs validation elements

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { NgIf } from '@angular/common';
+import { NgIf, NgFor } from '@angular/common';
 import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [RouterModule, ReactiveFormsModule, NgIf , NgClass],
+  imports: [RouterModule, ReactiveFormsModule, NgIf , NgClass, NgFor],
   providers: [FormBuilder, Validators],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss',
 })
 export class SignInComponent {
   signInForm: FormGroup;
+  formControls: { name: string, type: string, placeholder: string, minLength: number }[] = [
+    { name: 'username', type: 'text', placeholder: 'User-name', minLength: 3 },
+    { name: 'password', type: 'password', placeholder: 'Password', minLength: 8 },
+  ];
 
   constructor(private fb: FormBuilder) {
     this.signInForm = this.fb.group({
