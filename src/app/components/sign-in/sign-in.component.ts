99d784e22e6f26a8cc291e1af25c9f4578b1cf65
Arash-Azarpoor <azarpoor81@gmail.com>
2024-08-12 21:57:58 +0330

    Add input validator animations
    Co-authored-by: Armin <arminmow@gmail.com>

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { NgIf, NgFor } from '@angular/common';
 import { NgClass } from '@angular/common';
@@ -7,17 +12,29 @@ import { NgClass } from '@angular/common';
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [RouterModule, ReactiveFormsModule, NgIf , NgClass, NgFor],
+  imports: [RouterModule, ReactiveFormsModule, NgIf, NgClass, NgFor],
   providers: [FormBuilder, Validators],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss',
 })
 export class SignInComponent {
   signInForm: FormGroup;
-  formControls: { name: string, type: string, placeholder: string, minLength: number }[] = [
+  formControls: {
+    name: string;
+    type: string;
+    placeholder: string;
+    minLength: number;
+  }[] = [
     { name: 'username', type: 'text', placeholder: 'User-name', minLength: 3 },
-    { name: 'password', type: 'password', placeholder: 'Password', minLength: 8 },
+    {
+      name: 'password',
+      type: 'password',
+      placeholder: 'Password',
+      minLength: 8,
+    },
   ];
+  @ViewChildren('inputField') inputFields!: QueryList<ElementRef>;
+  isSubmitted: boolean = false;
 
   constructor(private fb: FormBuilder) {
     this.signInForm = this.fb.group({
@@ -27,8 +44,22 @@ export class SignInComponent {
   }
 
   onSubmit() {
+    this.isSubmitted = true;
     if (this.signInForm.valid) {
       console.log(this.signInForm.value);
+    } else {
+      this.signInForm.markAllAsTouched();
+      this.triggerShakeAnimation();
     }
+  }
+
+  private triggerShakeAnimation(): void {
+    this.inputFields.forEach((field) => {
+      const element = field.nativeElement;  
+      element.classList.remove('shake');
+      window.requestAnimationFrame(() => {
+        element.classList.add('shake');
+      });
+    });
   }
 }
