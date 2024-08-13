104fa8b8cbd059144900467354a6c51be388e51c
Arash-Azarpoor <azarpoor81@gmail.com>
2024-08-13 21:11:52 +0330

    Add dashboard components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,6 +13,7 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
+
   it(`should have the 'group4-front' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
