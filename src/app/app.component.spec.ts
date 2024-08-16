abc52ecf88d167b86566afd38eb2de5cdb7a121a
Armin <arminmow@gmail.com>
2024-08-16 05:46:50 +0330

    login tests and minor changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,17 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have the 'group4-front' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('group4-front');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, group4-front');
-  });
 });
--- src/app/services/login/login.service.spec.ts
+++ src/app/services/login/login.service.spec.ts
@@ -1,25 +1,36 @@
 import { TestBed } from '@angular/core/testing';
-
 import { LoginService } from './login.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpHandler } from '@angular/common/http';
 
 describe('LoginService', () => {
   let service: LoginService;
+  let httpClient: HttpClient;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [],
+      providers: [LoginService, HttpClient, HttpHandler],
+    });
     service = TestBed.inject(LoginService);
+    httpClient = TestBed.inject(HttpClient);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('SHOULD send proper data WHEN submited', () => {
+  it('SHOULD call post method with proper data WHEN submited', () => {
     // Arrange
-    
+    const spy = spyOn(httpClient, 'post').and.callThrough();
+    const apiUrl = 'http://192.168.24.180:5293/api/Auth/Login';
+    const body = { username: 'armin', password: '123' };
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     // Act
-    
+    service.login('armin', '123');
     // Assert
-    
+    expect(spy).toHaveBeenCalledWith(apiUrl, body, {
+      headers: jasmine.any(HttpHeaders),
+    });
   });
 });
