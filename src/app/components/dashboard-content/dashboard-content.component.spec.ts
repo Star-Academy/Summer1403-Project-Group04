692d3556b7ff427e129ad93392476a9e16cd2300
Armin <arminmow@gmail.com>
2024-08-24 16:27:44 +0330

    fix lint
    Co-authored-by: Arash <Arash-Azarpoor@users.noreply.github.com>

--- src/app/components/dashboard-content/dashboard-content.component.spec.ts
+++ src/app/components/dashboard-content/dashboard-content.component.spec.ts
@@ -1,16 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NZ_ICONS, NzIconService } from 'ng-zorro-antd/icon';
 import { DashboardContentComponent } from './dashboard-content.component';
+import { of } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { CommentOutline, DislikeOutline, LikeOutline } from '@ant-design/icons-angular/icons';
 
 describe('DashboardContentComponent', () => {
   let component: DashboardContentComponent;
   let fixture: ComponentFixture<DashboardContentComponent>;
+  const mockActivatedRoute = {
+    params: of({ id: 1 }),
+    snapshot: { params: { id: 1 } },
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DashboardContentComponent]
-    })
-    .compileComponents();
+      imports: [DashboardContentComponent],
+      providers: [
+        NzIconService,
+        {
+          provide: NZ_ICONS,
+          useValue: [CommentOutline, LikeOutline, DislikeOutline],
+        },
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(DashboardContentComponent);
     component = fixture.componentInstance;
