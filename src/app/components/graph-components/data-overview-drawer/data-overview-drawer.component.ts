18b4598dd08629dcc12f90cc27da8668f10f1b8f
Armin <arminmow@gmail.com>
2024-09-08 17:12:53 +0330

    added search params

--- src/app/components/graph-components/data-overview-drawer/data-overview-drawer.component.ts
+++ src/app/components/graph-components/data-overview-drawer/data-overview-drawer.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { NzDropdownMenuComponent, NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzDrawerModule } from 'ng-zorro-antd/drawer';
 import { NzInputModule } from 'ng-zorro-antd/input';
@@ -11,6 +11,8 @@ import { edgeData } from '../../../models/edge-data';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { GraphService } from '../../../services/graph/graph.service';
 import { SigmaService } from '../../../services/sigma/sigma.service';
+import { graphCategory } from '../../../models/graph-category';
+import { da, th } from '@faker-js/faker';
 
 @Component({
   selector: 'app-data-overview-drawer',
@@ -30,8 +32,12 @@ import { SigmaService } from '../../../services/sigma/sigma.service';
   templateUrl: './data-overview-drawer.component.html',
   styleUrl: './data-overview-drawer.component.scss',
 })
-export class DataOverviewDrawerComponent {
+export class DataOverviewDrawerComponent implements OnInit {
   constructor(private graphService: GraphService, private sigmaService: SigmaService) {}
+
+  ngOnInit(): void {
+   this.subscribeToServices();
+  }
 
   @Input() visible = false;
   @Input() selectedNode: nodeData | null = null;
@@ -43,7 +49,8 @@ export class DataOverviewDrawerComponent {
 
   private sourceNodeProp!: string[]; 
   private targetNodeProp!: string[];
-  private edgeProp!: string[];
+  private edgeProp!: string[]; 
+  private selectedCategories! : graphCategory; //Ino niaz nadarim chon tooye get-graph component line 68 be bad ok kardim
 
   close(): void {
     this.closeDrawer.emit();
@@ -51,6 +58,15 @@ export class DataOverviewDrawerComponent {
   
   search(): void {
     // Logic for searching the node
+    const searchParam = {
+      sourceCategoryName: this.selectedCategories.sourceCategoryName,
+      targetCategoryName: this.selectedCategories.targetCategoryName,
+      sourceCategoryClauses: this.sourceNodeProp,
+      targetCategoryClauses: this.targetNodeProp,
+      edgeCategoryClauses: this.edgeProp
+    }//I have no idea if this the right format
+
+    this.graphService.searchNode(searchParam)
   }
 
   expand(): void {
@@ -61,5 +77,29 @@ export class DataOverviewDrawerComponent {
   delete(): void {
     // Logic for deleting the node
     console.log('Delete clicked for node:', this.selectedNodeId);
+  }
+
+  subscribeToServices(){
+    // this.sigmaService.selectedGraphCategories$.subscribe({
+    //   next: (data)=>{
+    //     this.selectedCategories = data
+    //   }
+    // }) va hamintor in
+    this.sigmaService.sourceNodeProp$.subscribe({
+      next: (data)=>{
+        this.sourceNodeProp = data
+      }
+    })
+    this.sigmaService.targetNodeProp$.subscribe({
+      next: (data)=>{
+        this.targetNodeProp = data
+      }
+    })
+    this.sigmaService.edgeProp$.subscribe({
+      next: (data)=>{
+        this.edgeProp = data
+      }
+    })
+    
   }
 }
